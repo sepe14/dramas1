1b15b8843abd2db4dcbda83548754d2f015fd038
sepe14 <selley.pite@gmail.com>
2023-12-23 11:08:49 +0100

    init

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.lablec}>
+      <p>lábléc</p>
+    </footer>
+  );
+}
---
+++ src/components/navigation.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <nav className={styles.fejlec}>
+      <Link href="..">
+        <h1>Mkegnézett sorozatok listája</h1>
+      </Link>
+      <div>
+        <ul>
+          <li>
+            <Link href="/test">Teszt</Link>
+          </li>
+          <li>Alma2</li>
+          <li>Alma3</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
