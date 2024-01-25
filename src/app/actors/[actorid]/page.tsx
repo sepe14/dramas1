1b4cef8aa13ddbd5c5661e71fb9ce1640fc51814
sepe14 <selley.pite@gmail.com>
2024-01-25 16:26:20 +0100

    fix: actor was possibly null

--- src/app/actors/[actorid]/page.tsx
+++ src/app/actors/[actorid]/page.tsx
@@ -1,14 +1,18 @@
 import { prisma } from "@/db";
 
 function getActor(id: number) {
-    return prisma.actors.findUnique({
-      where: {
-        id: id,
-      },
-    });
-  }
+  return prisma.actors.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
 
-export default async function ActorPage({params}:{params: {actorid: string}}) {
-    const actor = await getActor(Number(params.actorid))
-    return <div>{actor.name}</div>
+export default async function ActorPage({
+  params,
+}: {
+  params: { actorid: string };
+}) {
+  const actor = await getActor(Number(params.actorid));
+  return <div>{actor?.name}</div>;
 }
